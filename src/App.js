e029b094fec63b61cc7d8cd5c03283858a595467
mir-hussain <mirhussainmurtaza@gmail.com>
2022-04-01 11:50:26 +0600

    first layer done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import "./App.css";
 import FirstComponent from "./Components/FirstComponent/FirstComponent";
 import SecondComponent from "./Components/SecondComponent/SecondComponent";
 import ThirdComponent from "./Components/ThirdComponent/ThirdComponent";
+import { GrFormAdd } from "react-icons/gr";
+import { AiOutlineMinus } from "react-icons/ai";
+import { useState, createContext } from "react";
+
+export const CounterContext = createContext();
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className='app'>
-      <div>
-        <h1>App</h1>
+    <CounterContext.Provider value={[count, setCount]}>
+      <div className='app'>
+        <div className='counter-container'>
+          <h1>App</h1>
+          <div>
+            <button title='Increase' onClick={() => setCount(count + 1)}>
+              <GrFormAdd size={20} />
+            </button>
+            <h1>Count: {count}</h1>
+            <button title='Decrease' onClick={() => setCount(count - 1)}>
+              <AiOutlineMinus size={20} />
+            </button>
+          </div>
+        </div>
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
       </div>
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
-    </div>
+    </CounterContext.Provider>
   );
 }
 
--- src/Components/FirstComponent/FirstComponent.js
+++ src/Components/FirstComponent/FirstComponent.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import FirstComponentChild from "../FirstComponentChild/FirstComponentChild";
+import { CounterContext } from "../../App";
+import { GrFormAdd } from "react-icons/gr";
+import { AiOutlineMinus } from "react-icons/ai";
 
 const FirstComponent = () => {
+  const [count, setCount] = useContext(CounterContext);
+
   return (
     <div className='first-component'>
-      <h1>First Component</h1>
+      <div className='counter-container'>
+        <h1>App</h1>
+        <div>
+          <button title='Increase' onClick={() => setCount(count + 1)}>
+            <GrFormAdd size={20} />
+          </button>
+          <h1>Count: {count}</h1>
+          <button title='Decrease' onClick={() => setCount(count - 1)}>
+            <AiOutlineMinus size={20} />
+          </button>
+        </div>
+      </div>
       <FirstComponentChild />
     </div>
   );
